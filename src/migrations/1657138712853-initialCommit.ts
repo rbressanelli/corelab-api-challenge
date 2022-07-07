5145cdf6d4959988d28d26f27724ad087a08bb17
Roberto Bressanelli <rbressanelli@gmail.com>
2022-07-07 08:30:56 -0300

    Feat: vehicle types and db config and migrations

---
+++ src/migrations/1657138712853-initialCommit.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner } from 'typeorm'
+
+export class initialCommit1657138712853 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
